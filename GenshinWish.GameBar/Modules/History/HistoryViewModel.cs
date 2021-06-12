c6958bec1557884642431d472ff0f1fe5ef74248
Ahmad <ahmadtantowi@outlook.com>
2021-06-12 19:04:13 +0700

    Implement storage permisson to access game log

--- GenshinWish.GameBar/Modules/History/HistoryViewModel.cs
+++ GenshinWish.GameBar/Modules/History/HistoryViewModel.cs
@@ -7,65 +7,77 @@ using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.Storage;
-using Windows.Storage.Pickers;
+using Windows.System;
+using Windows.UI.Popups;
 
 namespace GenshinWish.GameBar.Modules.History
 {
     public class HistoryViewModel : ObservableObject
     {
+        private const string _outputLog = "output_log.txt";
+
         private string _wishParam;
+        private MessageDialog _requestPermission;
 
         private readonly IApiService<IWish> _wishService;
+        private readonly string _gameDataPath;
 
         public HistoryViewModel()
         {
             _wishService = Ioc.Default.GetRequiredService<IApiService<IWish>>();
+            var dirInfo = new DirectoryInfo(ApplicationData.Current.LocalFolder.Path).Parent.Parent.Parent;
+            _gameDataPath = Path.Combine(dirInfo.FullName + "Low", "miHoYo", "Genshin Impact");
 
             FetchHistoryCommand = new AsyncRelayCommand(FetchHistory);
         }
 
         private async Task FetchHistory()
         {
-            var picker = new FileOpenPicker();
-            picker.FileTypeFilter.Add(".txt");
-            picker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
-            var file = await picker.PickSingleFileAsync();
-
-            using (var str = await file.OpenStreamForReadAsync())
+            try
             {
-                try
+                var log = await StorageFile.GetFileFromPathAsync(Path.Combine(_gameDataPath, _outputLog));
+                _wishParam = GenshinLog.GetWishParam(await log.OpenStreamForReadAsync());
+
+                var wishConfigQuery = GenshinLog.GetWishQueryString(WishEndpoint.Config, _wishParam);
+                var configResult = await _wishService.Initiated.GetWishConfig(wishConfigQuery);
+
+                foreach (var config in configResult.Data.GachaTypeList)
                 {
-                    _wishParam = GenshinLog.GetWishParam(str);
+                    Types.Add(config);
                 }
-                catch (Exception _)
+
+                var wishLogParam = GenshinLog.AddWishLogParam(_wishParam, 0, 6, configResult.Data.GachaTypeList.First().Key);
+                var wishLogQuery = GenshinLog.GetWishQueryString(WishEndpoint.Log, wishLogParam);
+                var logResult = await _wishService.Initiated.GetWishLog(wishLogQuery);
+
+                foreach (var wish in logResult.Data.List)
                 {
-                    
+                    Histories.Add(wish);
                 }
             }
-
-            var wishConfigQuery = GenshinLog.GetWishQueryString(WishEndpoint.Config, _wishParam);
-            var configResult = await _wishService.Initiated.GetWishConfig(wishConfigQuery);
-
-            foreach (var config in configResult.Data.GachaTypeList)
+            catch (UnauthorizedAccessException)
             {
-                Types.Add(config);
-            }
+                _requestPermission = _requestPermission ??
+                    new MessageDialog(
+                        $"The app needs to access the {_gameDataPath}. {Environment.NewLine}" +
+                        "Press OK to open system settings and give this app File System permission.");
+                var okCommand = new UICommand("Go to Settings");
+                _requestPermission.Commands.Add(okCommand);
+                var cancelCommand = new UICommand("Cancel");
+                _requestPermission.Commands.Add(cancelCommand);
 
-            var wishLogParam = GenshinLog.AddWishLogParam(_wishParam, 0, 6, configResult.Data.GachaTypeList.First().Key);
-            var wishLogQuery = GenshinLog.GetWishQueryString(WishEndpoint.Log, wishLogParam);
-            var logResult = await _wishService.Initiated.GetWishLog(wishLogQuery);
-
-            foreach (var wish in logResult.Data.List)
-            {
-                Histories.Add(wish);
+                var requestPermissionResult = await _requestPermission.ShowAsync();
+                if (requestPermissionResult == okCommand)
+                {
+                    //open app settings to allow users to give us permission
+                    await Launcher.LaunchUriAsync(new Uri("ms-settings:appsfeatures-app"));
+                }
             }
         }
 
